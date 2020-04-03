6e1d7a2b5f4ee182f5176a2880e9418747b9de7e
cesar214567 <cesar.madera@utec.edu.pe>
2020-04-03 08:01:26 -0500

    avance

---
+++ confirmedcovidcases/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,9 @@
+package cs_utec.cs2901;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ confirmedcovidcases/src/main/java/cs_utec/cs2901/data/DailyCases.java
@@ -0,0 +1,56 @@
+package cs_utec.cs2901.data;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table (name = "dailycases")
+public class DailyCases implements Serializable {
+
+    @Id
+    private Long id;
+
+    @Column
+    private Integer cantidad;
+
+    @Column
+    private Date fecha;
+
+    public DailyCases() {
+    }
+
+    public DailyCases(Long id, Integer cantidad, Date fecha) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}

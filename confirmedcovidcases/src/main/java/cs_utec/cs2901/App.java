9f4f3e914f852b28fa67ecf78a5008633bc8bc80
cesar214567 <cesar.madera@utec.edu.pe>
2020-05-13 10:08:44 -0500

    postgresql problem

--- confirmedcovidcases/src/main/java/cs_utec/cs2901/App.java
+++ confirmedcovidcases/src/main/java/cs_utec/cs2901/App.java
@@ -1,9 +1,19 @@
 package cs_utec.cs2901;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
